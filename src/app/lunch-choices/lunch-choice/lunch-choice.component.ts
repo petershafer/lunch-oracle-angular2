754eb13778eff36773ddd150633b7766085357e4
Peter Shafer <peter.shafer@tivix.com>
2016-06-26 15:12:37 -0700

    Add complex data passing for choices

--- src/app/lunch-choices/lunch-choice/lunch-choice.component.ts
+++ src/app/lunch-choices/lunch-choice/lunch-choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
@@ -11,6 +11,8 @@ import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
   directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_LIST_DIRECTIVES]
 })
 export class LunchChoiceComponent implements OnInit {
+
+  @Input() choice;
 
   constructor() {}
 
