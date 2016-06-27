a03434756f644a8f5fa2a295326e469fcb5a9301
Peter Shafer <peter.shafer@tivix.com>
2016-06-26 20:47:57 -0700

    Upgrade passing of option object

--- src/app/lunch-options/lunch-option/lunch-option.component.ts
+++ src/app/lunch-options/lunch-option/lunch-option.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { OptionsService } from '../../options.service';
 
 @Component({
   moduleId: module.id,
   selector: 'lunch-option',
   templateUrl: 'lunch-option.component.html',
   styleUrls: ['lunch-option.component.css'],
-  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES]
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES],
+  providers: [OptionsService]
 })
 export class LunchOptionComponent implements OnInit {
 
   constructor() {}
+  @Input() option;
+
+  active = false;
+
 
   ngOnInit() {
+  }
+
+  selected(option){
+    this.active = !this.active;
   }
 
 }
